76ceff79ad5bdf17bec02c5cb01f293057949361
Anubhav Chandak <anubhavchandak@gmail.com>
2025-06-26 05:20:33 +0530

    More responsiveness

--- frontend/src/utils/Layout.jsx
+++ frontend/src/utils/Layout.jsx
@@ -5,10 +5,10 @@ import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen w-full bg-[url('https://railmadad.indianrailways.gov.in/madad/final/images/body-bg.jpg')] bg-no-repeat bg-center bg-cover bg-fixed overflow-auto">
-      <div className="min-h-screen w-full flex flex-col">
+    <div className="min-h-screen min-w-[320px] w-full bg-[url('https://railmadad.indianrailways.gov.in/madad/final/images/body-bg.jpg')] bg-no-repeat bg-center bg-cover bg-fixed overflow-x-auto">
+      <div className="min-h-screen min-w-[320px] w-full flex flex-col relative">
         <ResponsiveNavbar />
-        <main className="flex-1 w-full">
+        <main className="flex-1 w-full min-w-[320px]">
           <Outlet />
         </main>
         <Footer />
